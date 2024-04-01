7c4fcbbc98d76ee355dbe5648611c6ae6f719e5a
Mikolkar <mikolaj.karapka@gmail.com>
2024-04-01 23:49:06 +0200

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Book Management
 
+## Description 
+This is a **Python** program built using the **Django** framework. It provides functionalities for managing books, including their borrowers and order history. Developed by **Mikołaj Karapka** for an **Advanced Python Programming** course, the application allows data population through either the **ORM** or an **API**.
+
+## Video Demonstration - TO DO
+
 ## Screenshots - TO DO
+
+## Features - TO DO
 
 ## Installation
 
@@ -9,10 +16,34 @@ poetry install
 ```
 
 ## Usage
+  #### To see all the commands and their usage patterns, simply type in the console:
 
-W celu zapoznania się ze wszystkimi operacjami i ich schematem użycia,
-wystarczy wpisać w konsoli python manage.py info.
+    python mge.py info
+
+or
 
 
-Program również posiada wizualizację danych w postaci graficznej. 
-Aby to uruchomić, wystarczy wpisać python manage.py runserver i przejść na stronę lokalną."
+    poetry run python manage.py info
+
+
+
+ #### The program also features data visualization in graphical form. 
+ To launch it, simply type:
+
+    
+    python mge.py runserver 
+    
+or
+
+    
+    poetry run python manage.py runserver
+    
+
+and navigate to the local website.
+
+
+#### You can also quick add to database some example data
+
+To launch it, simply type:
+
+    python mge.py example_data
--- lib/management/commands/info.py
+++ lib/management/commands/info.py
@@ -7,21 +7,37 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.stdout.write(
             """
-add_book - Dodaje książkę do biblioteki, 
-wywołanie: add_book <tytuł> <autor> <rok> / --api
+To use the following commands, first of all, you need to
+write python mge.py or poetry run python manage.py before the command. 
+for example: python mge.py add_book "title" "author" "year"
 
-add_friend - Dodaje znajomego do biblioteki,
-wywołanie: add_friend <imię> <email> / --api
+To use the API, add --api at the end of the command. 
+for example: python mge.py add_book "title" "author" "year" --api
 
-borrow - Wypożycza książkę znajomemu,
-wywołanie: borrow <id książki> <id znajomego> / --api
+Before this you need to run the server.
+python mge.py runserver
 
-list <books|friends|borrows> - Wyświetla listę książek, znajomych lub wypożyczeń,
+Commands:
 
-rm <book/friend> - Usuwa książkę z biblioteki,
-wywołanie: rm <book/friend> <id>  / --api
+add_book - Adds a book to the library,
+usage: add_book <title> <author> <year> / --api
 
-return - Oznacza książkę jako zwróconą,
-wywołanie: return <id wypożyczenia> / --return_date=<date> / --api
+add_friend - Adds a friend to the library,
+usage: add_friend <Name> <email> / --api
+
+borrow - Borrows a book for a friend,
+usage: borrow <book id> <friend id> / --api
+
+list - Lists of books, friends, or borrows,
+usage: list <books|friends|borrows> - Lists of books, friends, or borrows,
+
+rm <book/friend> - Removes a book or friend from the library,
+usage: rm <book/friend> <id> / --api
+
+return - Returns a borrowed book,
+usage: return <borrow id> / --return_date=<date> / --api
+
+flush - Removes all data from the database,
+usage: flush
     """
         )
